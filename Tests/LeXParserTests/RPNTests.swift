090d446974b168f643f5871950a7d0bdc406a6fc
Wojciech Kosikowski <83952389+WKosikowski@users.noreply.github.com>
2024-10-27 17:21:38 +0000

    Added New RPNTests Test
    Testing for brackets inside brackets.

--- Tests/LeXParserTests/RPNTests.swift
+++ Tests/LeXParserTests/RPNTests.swift
@@ -43,6 +43,19 @@ struct RPNTests {
             Token(value: "*", type: .Operator),
             Token(value: "+", type: .Operator)
         ]),
+        RPNTestData(input: "out1=a+b+(d*(c+f))", output: [
+            Token(value: "out1", type: .Operand),
+            Token(value: "=", type: .Equal),
+            Token(value: "a", type: .Operand),
+            Token(value: "b", type: .Operand),
+            Token(value: "+", type: .Operator),
+            Token(value: "d", type: .Operand),
+            Token(value: "c", type: .Operand),
+            Token(value: "f", type: .Operator),
+            Token(value: "+", type: .Operator),
+            Token(value: "*", type: .Operator),
+            Token(value: "+", type: .Operator)
+        ]),
     ])
     func testRPN(data: RPNTestData) async throws {
         let parser = Parser()
